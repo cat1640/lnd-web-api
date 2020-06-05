9ce281b955fd0cb728a6d118651ccc160ddd3ff5
cat1640 <catherine.swale@vgw.co>
2020-06-05 16:14:30 +0800

    Add mocha and nyc

---
+++ src/wallet/wallet.js
@@ -0,0 +1,4 @@
+function createWallet() {
+    const wallet = {}
+}
+
---
+++ src/wallet/wallet.test.js
@@ -0,0 +1,11 @@
+
+describe("Color Code Converter", function() {
+    describe("RGB to Hex conversion", function() {
+        // specification for RGB to HEX converter
+
+    });
+
+    describe("Hex to RGB conversion", function() {
+        // specification for HEX to RGB converter
+    });
+});
